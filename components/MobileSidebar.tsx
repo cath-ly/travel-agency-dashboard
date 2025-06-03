90396168c77b4c04422354b40e706144fc972877
cath-ly <takath339@gmail.com>
2025-06-03 13:53:53 -0400

    Added Mobile Sidebar

---
+++ components/MobileSidebar.tsx
@@ -0,0 +1,44 @@
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import { Link, Links } from "react-router";
+import NavItems from "./NavItems";
+
+const MobileSidebar = () => {
+  let sidebar: SidebarComponent;
+  const toggleSidebar = () => {
+    sidebar.toggle();
+  };
+
+  return (
+    <div className="mobile-sidebar wrapper">
+      <header>
+        <Link to="/">
+          <img
+            src="/assets/images/logo.png"
+            alt="Logo"
+            className="size-[30px]"
+          />
+
+          <h1>Grand Sekai</h1>
+        </Link>
+
+        <button onClick={toggleSidebar}>
+          <img src="/assets/icons/menu.svg" alt="Menu" className="size-7" />
+        </button>
+      </header>
+
+      <SidebarComponent
+        //@ts-ignore
+        width={270}
+        //@ts-ignore
+        ref={(Sidebar) => (sidebar = Sidebar)}
+        created={() => sidebar.hide()}
+        closeOnDocumentClick={true}
+        showBackdrop={true}
+        type="over">
+        <NavItems handleClick={toggleSidebar} />
+      </SidebarComponent>
+    </div>
+  );
+};
+
+export default MobileSidebar;
