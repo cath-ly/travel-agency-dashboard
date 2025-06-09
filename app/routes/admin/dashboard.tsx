915d3cb7883eef151cc77d57b2e739eae446a2a6
Chantakrak Ath-Ly <takath339@gmail.com>
2025-06-09 15:23:08 -0400

    Implemented mock data and StatCard

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,7 +1,15 @@
-import { Header } from "components";
+import { Header, StatCard, TripCard } from "components";
 
 const Dashboard = () => {
   const user = { name: "Chantakrak" };
+  const dashboardStat = {
+    totalUsers: 10,
+    monthlyUsers: { currentMonth: 8, lastMonth: 2},
+    totalTrips: 9,
+    monthlyTrips: { currentMonth: 5, lastMonth: 4},
+    activeUsers: { total: 9, currentMonth: 4, lastMonth: 2},
+  }
+  const {totalUsers, monthlyUsers, totalTrips, monthlyTrips, activeUsers } = dashboardStat;
 
   return (
     <main className="dashboard wrapper">
@@ -10,6 +18,29 @@ const Dashboard = () => {
         description="Track current activities, trends, and popular destinations"
       />
       Dashboard Page Content
+      <section className="flex flex-col gap-6">
+        <div className="grid grid-cols-1 md: grid-cols-3 gap-6 w-full">
+          <StatCard 
+            headerTitle="Total Users"
+            total={totalUsers}
+            currentMonthTotal={monthlyUsers.currentMonth}
+            lastMonthTotal={monthlyUsers.lastMonth}
+          />
+          <StatCard 
+            headerTitle="Total Trips"
+            total={totalTrips}
+            currentMonthTotal={monthlyTrips.currentMonth}
+            lastMonthTotal={monthlyTrips.lastMonth}
+          />
+          <StatCard 
+            headerTitle="Active Users"
+            total={activeUsers.total}
+            currentMonthTotal={activeUsers.currentMonth}
+            lastMonthTotal={activeUsers.lastMonth}
+          />
+        </div>
+      </section>
+      <TripCard />
     </main>
   );
 };
