6a16dc24576a6cb43bbab8726dccffc125c4e76d
cath-ly <takath339@gmail.com>
2025-06-30 16:44:52 -0400

    Created formatKey util fn

---
+++ app/lib/utils/formatKey.ts
@@ -0,0 +1,7 @@
+const formatKey = (key: keyof TripFormData) => {
+  return key
+    .replace(/([A-Z])/g, " $1")
+    .replace(/^./, (str) => str.toUpperCase());
+};
+
+export default formatKey;
--- app/lib/utils/index.ts
+++ app/lib/utils/index.ts
@@ -2,3 +2,4 @@ export { default as calculateTrendPercentage } from "./calculateTrendPercentage"
 export { default as cn } from "./cn";
 export { default as formatDate } from "./formatDate";
 export { default as getFirstWord } from "./getFirstWord";
+export { default as formatKey } from "./formatKey";
