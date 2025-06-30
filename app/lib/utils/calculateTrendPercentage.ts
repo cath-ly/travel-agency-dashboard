2797b204d6e3e245672bb14d4f35ea36bf6b2e77
cath-ly <takath339@gmail.com>
2025-06-30 16:44:37 -0400

    Resolved redundant export

--- app/lib/utils/calculateTrendPercentage.ts
+++ app/lib/utils/calculateTrendPercentage.ts
@@ -1,21 +1,23 @@
-export const calculateTrendPercentage = (currentMonthTotal: number, lastMonthTotal: number): TrendResult => {
-    if (lastMonthTotal === 0){
-        return currentMonthTotal === 0 
-            ? { trend: "no change", percentage: 0} 
-            : { trend: "increment", percentage: 100};
-    }
+const calculateTrendPercentage = (
+  currentMonthTotal: number,
+  lastMonthTotal: number
+): TrendResult => {
+  if (lastMonthTotal === 0) {
+    return currentMonthTotal === 0
+      ? { trend: "no change", percentage: 0 }
+      : { trend: "increment", percentage: 100 };
+  }
 
-    const change = currentMonthTotal - lastMonthTotal;
-    const percentage = change/lastMonthTotal * 100;
+  const change = currentMonthTotal - lastMonthTotal;
+  const percentage = (change / lastMonthTotal) * 100;
 
-    if (change > 0){
-        return { trend: "increment", percentage};
-    }
-    else if (change < 0){
-        return { trend: "decrement", percentage: Math.abs(percentage)};
-    }
+  if (change > 0) {
+    return { trend: "increment", percentage };
+  } else if (change < 0) {
+    return { trend: "decrement", percentage: Math.abs(percentage) };
+  }
 
-    return { trend: "no change", percentage: 0};
-}
+  return { trend: "no change", percentage: 0 };
+};
 
 export default calculateTrendPercentage;
