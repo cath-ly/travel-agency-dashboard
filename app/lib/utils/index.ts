25981a5c71b3ae946ff983999fa6c15c8e680ea7
cath-ly <takath339@gmail.com>
2025-07-15 16:35:21 -0400

    Added new util function parseMarkdownJSON

--- app/lib/utils/index.ts
+++ app/lib/utils/index.ts
@@ -3,3 +3,4 @@ export { default as cn } from "./cn";
 export { default as formatDate } from "./formatDate";
 export { default as getFirstWord } from "./getFirstWord";
 export { default as formatKey } from "./formatKey";
+export { default as parseMarkdownToJSON } from "./parseMarkdownToJSON";
