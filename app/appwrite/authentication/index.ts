c87a9a8ef31ee6aaee86b53176bab59d46a02bc7
cath-ly <takath339@gmail.com>
2025-07-18 14:57:54 -0400

    Updated directories to maintain consistent name convention

--- app/appwrite/authentication/index.ts
+++ app/appwrite/authentication/index.ts
@@ -1,7 +1,3 @@
 export { default as loginWithGoogle } from "./loginWithGoogle";
-export { default as getUser } from "./getUser";
-export { default as getGooglePicture } from "./getGooglePicture";
 export { default as logoutUser } from "./logoutUser";
 export { default as storeUserData } from "./storeUserData";
-export { default as getExistingUser } from "./getExistingUser";
-export { default as getUsers } from "./getUsers";
---
+++ app/appwrite/collect/index.ts
@@ -0,0 +1,5 @@
+export { default as getUser } from "./getUser";
+export { default as getGooglePicture } from "./getGooglePicture";
+export { default as getExistingUser } from "./getExistingUser";
+export { default as getUsers } from "./getUsers";
+export { default as getAllTrips } from "./getAllTrips";
