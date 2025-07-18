9eaf046afb25272c0d16670acf74e4a3e404628f
cath-ly <takath339@gmail.com>
2025-07-18 15:05:01 -0400

    Typo from previous commit

--- app/routes.ts
+++ app/routes.ts
@@ -1,12 +1,13 @@
 import { type RouteConfig, route, layout } from "@react-router/dev/routes";
 
 export default [
-  route("sign-in", "routes/root/sign-in.tsx"),
-  route("api/create-trip", "routes/api/create-trip.ts"),
+  route("sign-in", "routes/root/signIn.tsx"),
+  route("api/create-trip", "routes/api/createTrip.ts"),
   layout("routes/admin/adminLayout.tsx", [
     route("dashboard", "routes/admin/dashboard.tsx"),
     route("users", "routes/admin/userLayout.tsx"),
     route("trips", "routes/admin/trips.tsx"),
     route("trips/create", "routes/admin/createTrip.tsx"),
+    route("trips/:tripId", "routes/admin/tripDetail.tsx"),
   ]),
 ] satisfies RouteConfig;
