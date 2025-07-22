eadf7391c5bf42838c1ba7bd5cdeeb904dac05ff
cath-ly <takath339@gmail.com>
2025-07-22 13:19:07 -0400

    Renamed getTripByID

--- app/appwrite/collect/index.ts
+++ app/appwrite/collect/index.ts
@@ -3,3 +3,4 @@ export { default as getGooglePicture } from "./getGooglePicture";
 export { default as getExistingUser } from "./getExistingUser";
 export { default as getUsers } from "./getUsers";
 export { default as getAllTrips } from "./getAllTrips";
+export { default as getTripByID } from "./getTripByID";
